426b63be8b972658467433c8b00b584d419a2256
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-23 21:55:13 +0300

    Tree class added'

---
+++ ForestFire/Tree.cs
@@ -0,0 +1,25 @@
+﻿namespace ForestFire
+{
+    public class Tree
+    {
+        public int LifePoints{ get; set; }
+        public char Status { get; set; }
+        public Tree(int lifePoints)
+        {
+            LifePoints = lifePoints;
+            Status = 'o';
+        }
+        public void Burn()
+        {
+            LifePoints--;
+            if (LifePoints == 0)
+                Status = '.';
+            else
+                Status = 'X';
+        }
+        public bool IsBurned()
+        {
+            return Status == '.';
+        }
+    }
+}
