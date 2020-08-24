a55f6cb6534ff52ee4885369a635637d62547155
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-24 15:18:08 +0300

    GetNearTrees function at Forest class implemeted

--- ForestFire/Program.cs
+++ ForestFire/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ForestFire
 {
@@ -7,6 +8,39 @@ namespace ForestFire
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    public class Forest
+    {
+        public Tree[,] TreeMatriza { get; set; }
+        public Forest(Tree[,] trees)
+        {
+            TreeMatriza = trees;
+        }
+        public void FillForest()
+        {
+
+        }
+        public List<Tree> GetNearTrees(int xCoordinate, int yCoordinate)
+        {
+            List<Tree> nearTrees = new List<Tree>();
+            for (int x = xCoordinate-1; x<=xCoordinate+1; x++)
+            {
+                for (int y = yCoordinate - 1; y <= yCoordinate + 1; y++)
+                {
+                    try
+                    {
+                        Tree t1 = TreeMatriza[x, y];
+                        if (t1 != null)
+                            nearTrees.Add(t1);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            return nearTrees;
         }
     }
 }
